612d7327519f6a2307d209bf6e34ea9955f5fef0
Silvano <slopez@vise.com.mx>
2018-03-09 21:03:28 -0600

    Nuevo Repositorio

---
+++ action.js
@@ -0,0 +1,24 @@
+function departamentos(){
+  var boxes = ["MAQUINARIA",
+  "CAPITAL HUMANO",
+  "CONCESIONES",
+  "CONTROL INTERNO",
+  "OBRA CIVIL",
+  "COMPRAS",
+  "PLANTAS ASFALTOS",
+  "TRITURADOS",
+  "CONTABILIDAD",
+  "INMOBILIARIA",
+  "MERCADOTECNIA",
+  "TECNOLOGIAS DE INFORMACION"];
+  var contenido = "";
+
+  for (var i = 0; i < boxes.length; i++){
+    contenido += "<div class='cubes'>" + boxes[i] + "</div>";
+  }
+  document.getElementById('content').innerHTML = contenido;
+}
+
+function saludo(){
+    document.getElementById('dialogo').innerHTML = "<h1> Hola! </h1>";
+}
